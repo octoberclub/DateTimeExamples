c7f6ccb4e15d9f649249020dd644fcee92a1d1ed
octoberclub <michelle.steele@15below.com>
2014-04-03 23:33:37 +0100

    Initial Examples

---
+++ DateTimeExamples/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Now
+            var d = DateTime.Now;
+            Console.WriteLine("{0}, {1}", d, d.Kind);
+
+            // Parse a Date
+            var d1 = DateTime.Parse("2014-03-30 01:30", CultureInfo.CurrentCulture, DateTimeStyles.AssumeLocal);
+            Console.WriteLine("{0}, {1}", d1, d.Kind);
+
+            // Convert to Utc
+            var dUtc = TimeZoneInfo.ConvertTimeToUtc(d, TimeZoneInfo.Local);
+            Console.WriteLine("{0}, {1}", dUtc, dUtc.Kind);
+
+            // Convert invalid date to Utc
+            var d2Utc = TimeZoneInfo.ConvertTimeToUtc(d, TimeZoneInfo.Local);
+            Console.WriteLine("{0}, {1}", d2Utc, d2Utc.Kind);
+
+            // Arithmetic - eg. duration
+            var d3 = DateTime.Parse("2014-03-30", CultureInfo.CurrentCulture, DateTimeStyles.AssumeLocal);
+            var d4 = DateTime.Parse("2014-03-31", CultureInfo.CurrentCulture, DateTimeStyles.AssumeLocal);
+            var duration = d4 - d3;
+            Console.WriteLine("{0}, {1}, {2}", d3, d4, duration.TotalHours);
+
+            // Show Hours
+            var d5 = DateTime.Parse("2014-03-30", CultureInfo.CurrentCulture, DateTimeStyles.AssumeLocal);
+            for (int hour = 0; hour < 24; hour++)
+            {
+                var d6 = d5.AddHours(hour);
+                Console.WriteLine("{0}", d6);
+                TimeZoneInfo.ConvertTimeToUtc(d6, TimeZoneInfo.Local);
+            }
+             
+            // Show Last Day of month
+            var d7 = DateTime.Parse("2014-01-31", CultureInfo.CurrentCulture, DateTimeStyles.AssumeLocal);
+            for (int month = 0; month < 12; month++)
+            {
+                var d8 = d7.AddMonths(month);
+                Console.WriteLine("{0}", d8);
+            }
+             
+            Console.WriteLine("Press a key");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ NodaTimeExample/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NodaTime;
+
+namespace NodaTimeExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //// Now Instance
+            //var now = SystemClock.Instance.Now;
+            //Console.WriteLine("{0}, {1}", now, now.GetType());
+
+            //// Date to Utc LocalDateTime
+            //var tz = DateTimeZoneProviders.Tzdb["Europe/London"];
+            //var date=new LocalDateTime(2014, 3, 30, 1, 5);
+            //var tzd1 = tz.AtLeniently(date);
+            //var tzd2 = tzd1.ToInstant().ToDateTimeUtc();
+            //Console.WriteLine("{0},{1}", tzd1, tzd2);
+
+            //// Date Arithmetic Period
+            //var d1 = new LocalDateTime(2014, 3, 30, 0, 0);
+            //var d2 = new LocalDateTime(2014, 3, 31, 0, 0);
+            //var duration = Period.Between(d1, d2).ToDuration();
+            //Console.WriteLine("{0}, {1}", duration, duration.GetType());
+
+            //// OffsetDateTime
+            //var od = new OffsetDateTime(d1, Offset.FromHours(1));
+            //Console.WriteLine(od.LocalDateTime);
+
+            // Show Hours
+            //var tz = DateTimeZoneProviders.Tzdb["Europe/London"];
+            //var date = new LocalDateTime(2014, 3, 30, 0, 0);
+            //var tzd1=new ZonedDateTime(date, tz, Offset.FromHours(0));
+            //for (int hour = 0; hour < 24; hour++)
+            //{
+            //    var d6 = tzd1.Plus(Duration.FromHours(hour));
+            //    Console.WriteLine("{0}", d6);
+            //}
+
+            //// Show Last Day of month
+            //var dm = new LocalDateTime(2014, 1, 31, 0, 0);
+            //for (int month = 0; month < 12; month++)
+            //{
+            //    var d3 = dm.PlusMonths(month);
+            //    Console.WriteLine("{0}", d3);
+            //}
+
+            Console.WriteLine("Press a key");
+            Console.ReadKey();
+        }
+    }
+}
